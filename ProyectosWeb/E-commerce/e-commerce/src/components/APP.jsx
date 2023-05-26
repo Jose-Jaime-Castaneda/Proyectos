8e604bfb96a7365fdf99f247d890ca971f1467f5
JoseCastañeda <77752153+josejaimrgcr@users.noreply.github.com>
2023-05-26 16:37:17 -0600

    Fetch API completa

--- ProyectosWeb/E-commerce/e-commerce/src/components/APP.jsx
+++ ProyectosWeb/E-commerce/e-commerce/src/components/APP.jsx
@@ -1,10 +1,25 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "../css/APP.css";
 
 const App = () => {
+  // Variable para probar fetch API
+  const [aux, setAux] = useState([]);
+  // Fecth API
+  useEffect(() => {
+    fetch("http://localhost:3001/categories")
+      .then((reponse) => reponse.json())
+      .then((info) => {
+        console.log(info);
+        setAux(info);
+      });
+  }, []);
+
   return (
     <main>
-      <h1>Hola bb</h1>
+      <h1>Categorias</h1>
+      {aux.map((e) => (
+        <p key={e.id}>{e.title}</p>
+      ))}
     </main>
   );
 };
