f38cf70c5aeecbf76884040e3091a0eb2b80e7e1
JoseCastañeda <77752153+josejaimrgcr@users.noreply.github.com>
2023-05-26 15:46:57 -0600

    Primer componente y hoja de estilo

---
+++ ProyectosWeb/E-commerce/e-commerce/src/components/APP.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import "../css/APP.css";
+
+const App = () => {
+  return (
+    <main>
+      <h1>Hola bb</h1>
+    </main>
+  );
+};
+
+export default App;
--- ProyectosWeb/E-commerce/e-commerce/src/index.js
+++ ProyectosWeb/E-commerce/e-commerce/src/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import App from "./components/APP";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <h1>Hola bb</h1>
+    <App />
   </React.StrictMode>
 );
