19758a6aadcb762c3d0bddc008870f18d5df82c5
JoseCastañeda <77752153+josejaimrgcr@users.noreply.github.com>
2023-05-29 17:23:07 -0600

    Mostramos los productos

--- ProyectosWeb/E-commerce/e-commerce/src/components/Main/Main.jsx
+++ ProyectosWeb/E-commerce/e-commerce/src/components/Main/Main.jsx
@@ -2,21 +2,28 @@ import React, { useState, useEffect } from "react";
 import "../../css/Main.css";
 
 const Main = () => {
+  // Constante con la URL
+  const url = "http://localhost:3001";
   // Variable para probar fetch API
   const [categorias, setCategorias] = useState([]);
+  const [productos, setProductos] = useState([]);
   // Fecth API
   useEffect(() => {
-    fetch("http://localhost:3001/categorias")
+    fetch(url + "/categorias")
       .then((reponse) => reponse.json())
       .then((info) => {
         // console.log(info);
         setCategorias(info);
       });
   }, []);
-  // Funciones para manejar los eventos clic de las categorias
-  // Funcion para las laptops
-  const handleLaptops = (id) => {
-    alert("id de la catogeria: " + id);
+  // Funcion para manejar los eventos clic de las categorias
+  const handleProductos = (id) => {
+    fetch(url + "/productos?catId=" + id)
+      .then((reponse) => reponse.json())
+      .then((info) => {
+        // console.log(info);
+        setProductos(info);
+      });
   };
 
   return (
@@ -24,13 +31,16 @@ const Main = () => {
       <aside className="Aside">
         <h3>Categorias</h3>
         {categorias.map((e) => (
-          <p onClick={() => handleLaptops(e.id)} key={e.id}>
+          <p onClick={() => handleProductos(e.id)} key={e.id}>
             {e.title}
           </p>
         ))}
       </aside>
       <main className="Main">
-        <h1>Contenido Principal</h1>
+        <h1>Productos</h1>
+        {productos.map((l) => (
+          <p key={l.id}>{l.title}</p>
+        ))}
       </main>
     </div>
   );
