603b1d6eb3f7ac2a3c6f99ca2c352c631cbad18c
JoseCastañeda <77752153+josejaimrgcr@users.noreply.github.com>
2023-05-31 13:40:06 -0600

    Arreglamos el react router

--- ProyectosWeb/E-commerce/e-commerce/src/components/APP.jsx
+++ ProyectosWeb/E-commerce/e-commerce/src/components/APP.jsx
@@ -1,54 +1,11 @@
-import React, { useEffect } from "react";
-import { Routes, Route, useNavigate } from "react-router-dom";
+import React from "react";
 import "../css/APP.css";
-import Header from "../components/Header/Header";
-import Footer from "../components/Footer/Footer";
 import Main from "./Main/Main";
-import Carrito from "./Carrito/Carrito";
-import About from "./About/About";
-import NotFound from "./Error/NotFound";
 
 const App = () => {
-  const navegar = useNavigate();
-  useEffect(() => {
-    navegar("/home");
-  }, []);
-
   return (
     <div className="MainDiv">
-      <Routes>
-        <Route
-          path="/home"
-          element={
-            <>
-              <Header />
-              <Main />
-              <Footer />
-            </>
-          }
-        />
-        <Route
-          path="/carrito"
-          element={
-            <>
-              <Header />
-              <Carrito />
-              <Footer />
-            </>
-          }
-        />
-        <Route
-          path="/about"
-          element={
-            <>
-              <Header />
-              <About />
-              <Footer />
-            </>
-          }
-        />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      <Main />
     </div>
   );
 };
--- ProyectosWeb/E-commerce/e-commerce/src/index.js
+++ ProyectosWeb/E-commerce/e-commerce/src/index.js
@@ -1,14 +1,53 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
 import App from "./components/APP";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import Carrito from "./components/Carrito/Carrito";
+import About from "./components/About/About";
+import NotFound from "./components/Error/NotFound";
+import ProdutosInfo from "./components/ProdcutosInfo/ProductosInfo";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter >
-      <App />
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/home"
+          element={
+            <>
+              <Header />
+              <App />
+              <Footer />
+            </>
+          }
+        />
+        <Route
+          path="/carrito"
+          element={
+            <>
+              <Header />
+              <Carrito />
+              <Footer />
+            </>
+          }
+        />
+        <Route
+          path="/about"
+          element={
+            <>
+              <Header />
+              <About />
+              <Footer />
+            </>
+          }
+        />
+        <Route path="/producto/:productoID" element={<ProdutosInfo />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </BrowserRouter>
   </React.StrictMode>
 );
