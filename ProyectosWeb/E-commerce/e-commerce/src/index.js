d57df05d01b5972da5dae99441a1f467c597853b
JoseCastañeda <77752153+josejaimrgcr@users.noreply.github.com>
2023-05-31 14:01:58 -0600

    Extras

--- ProyectosWeb/E-commerce/e-commerce/src/index.js
+++ ProyectosWeb/E-commerce/e-commerce/src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
-import App from "./components/APP";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import Carrito from "./components/Carrito/Carrito";
