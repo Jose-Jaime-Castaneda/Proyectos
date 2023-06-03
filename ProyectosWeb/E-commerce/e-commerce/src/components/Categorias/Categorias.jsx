bd6b9b7664c2bbc190019c99bf449a18721dec82
JoseCastañeda <77752153+josejaimrgcr@users.noreply.github.com>
2023-06-02 20:05:23 -0600

    Estilos de categoria listos

--- ProyectosWeb/E-commerce/e-commerce/src/components/Categorias/Categorias.jsx
+++ ProyectosWeb/E-commerce/e-commerce/src/components/Categorias/Categorias.jsx
@@ -1,9 +1,13 @@
 import React, { useState, useEffect } from "react";
+import "../../css/Categorias.css";
 import { getCategorias } from "../../Utils";
 import { Link } from "react-router-dom";
 
 const Categorias = () => {
+  // Estado para almacenar las categorias
   const [categorias, setCategorias] = useState([]);
+  // Hook para obtener las categorias cuando
+  // se monte el componente
   useEffect(() => {
     const fetchData = async () => {
       const data = await getCategorias();
@@ -11,15 +15,18 @@ const Categorias = () => {
     };
     fetchData();
   }, []);
+
   return (
     <aside className="Aside">
       <h3>Categorias</h3>
-      {categorias &&
-        categorias.map((e) => (
-          <Link to={`/productosByCat/${e.id}`} key={e.id}>
-            {e.title}
-          </Link>
-        ))}
+      <div className="navLink">
+        {categorias &&
+          categorias.map((e) => (
+            <Link className="link" to={`/productosByCat/${e.id}`} key={e.id}>
+              {e.title}
+            </Link>
+          ))}
+      </div>
     </aside>
   );
 };
