8fab90c4505d378ff00215558ddf309a842a9271
JoseCastañeda <77752153+josejaimrgcr@users.noreply.github.com>
2023-06-06 16:22:38 -0600

    Implementacion de los casos para el reductor

--- ProyectosWeb/E-commerce/e-commerce/src/components/Context/cartReducer.jsx
+++ ProyectosWeb/E-commerce/e-commerce/src/components/Context/cartReducer.jsx
@@ -1,24 +1,37 @@
 export const CartReducer = (estado, accion) => {
   debugger;
+  let index = -1;
+
+  if (accion.infoProd)
+    index = estado.cartItem.findIndex((i) => i.id === accion.infoProd.id);
 
   switch (accion.type) {
     case "ADD":
-      const index = estado.cartItem.findIndex(
-        (i) => i.id === accion.infoProd.id
-      );
-
+    case "INCCANT":
       if (index === -1) {
         estado.cartItem.push({ ...accion.infoProd, cantidad: 1 });
       } else {
         estado.cartItem[index].cantidad++;
       }
-
-      return estado;
+      break;
 
     case "REMOVE":
-      return estado;
+      if (index > -1) {
+        estado.cartItem.splice(index, 1);
+      }
+      break;
+
+    case "DECCANT":
+      if (index > -1) {
+        estado.cartItem[index].cantidad--;
+      }
+      break;
+
+    case "CLEAR":
+      estado.cartItem = [];
+      break;
 
     default:
-      return estado;
   }
+  return estado;
 };
