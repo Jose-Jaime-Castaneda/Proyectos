baa35007b2d089d882953e263d592ac2d6ec127c
JoseCastañeda <77752153+josejaimrgcr@users.noreply.github.com>
2023-06-05 18:20:45 -0600

    Quitamos importaciones innecesarias

--- ProyectosWeb/E-commerce/e-commerce/src/index.js
+++ ProyectosWeb/E-commerce/e-commerce/src/index.js
@@ -1,16 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
-import Header from "./components/Header/Header";
-import Footer from "./components/Footer/Footer";
-import Carrito from "./components/Carrito/Carrito";
-import About from "./components/About/About";
-import NotFound from "./components/Error/NotFound";
-import ProdutosInfo from "./components/ProdcutosInfo/ProductosInfo";
-import Main from "./components/Main/Main";
-import Producto from "./components/Productos/Producto";
-import Categorias from "./components/Categorias/Categorias";
 import Navigation from "./components/Navigation/Navigation";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
