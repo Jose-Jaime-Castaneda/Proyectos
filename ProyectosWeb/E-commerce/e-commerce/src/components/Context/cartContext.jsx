d1dd17e7abec41c28a92409687328dfd0359d47f
JoseCastañeda <77752153+josejaimrgcr@users.noreply.github.com>
2023-06-06 15:45:41 -0600

    Modificacion de los comentarios

--- ProyectosWeb/E-commerce/e-commerce/src/components/Context/cartContext.jsx
+++ ProyectosWeb/E-commerce/e-commerce/src/components/Context/cartContext.jsx
@@ -1,21 +1,18 @@
 import React, { createContext, useReducer } from "react";
 import { CartReducer } from "./cartReducer";
 // Constante que usaremos para crear contexto
-// y manejar los estados del carrito
 export const context = createContext();
-// Objeto que guardará los articulos en al carrito
+// Estado inicial del objeto cartItem
 const estadoInicial = { cartItem: [] };
-// Componente que usaremos para cambiar el arbol de
-// componentes en la estrcutura del proyecto
+// Componente que usaremos como provedor de contexto
 const CartContextProvider = ({ children }) => {
-  // Estado para cambiar con el Reducer
+  // Estado para actualizar con el Reducer
   const [state, dispatch] = useReducer(CartReducer, estadoInicial);
   // Función que guarda los articulos en el carro
   const agregarProducto = (infoProd) => {
     dispatch({ type: "ADD", infoProd });
   };
-  // Función que renderiza la información del estado
-  // con los productos en el carrito
+  // Función que renderiza la información del estado actualizado
   const contextData = {
     agregarProducto,
     ...state,
