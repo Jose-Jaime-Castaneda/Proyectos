fc8cac9f62a7ee8c9d72036a9abea5c8a675791f
JoseCastañeda <77752153+josejaimrgcr@users.noreply.github.com>
2023-06-10 14:42:20 -0600

    ComprarCarrito responsive listo

--- ProyectosWeb/E-commerce/e-commerce/src/components/ComprarCarrito/comprarCarrito.jsx
+++ ProyectosWeb/E-commerce/e-commerce/src/components/ComprarCarrito/comprarCarrito.jsx
@@ -1,7 +1,34 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "../../css/ComprarCarrito.css";
 
 const ComprarCarrito = () => {
-  return <h1>Hola desde comprarCarrito</h1>;
+  return (
+    <section className="sectionComprar">
+      <h3>Formulario de compra</h3>
+      <form>
+        <h5>Datos del usuario</h5>
+        <div className="infoUsu">
+          <label className="label">Nombre</label>
+          <input type="text" placeholder="Nombre de usuario" />
+          <label>Correo</label>
+          <input type="email" placeholder="ejem@gmail.com" />
+        </div>
+        <h5>Dirección</h5>
+        <div className="direccionUsu">
+          <label>Estado y Municio</label>
+          <input type="text" placeholder="Aguascalientes, Aguascalientes" />
+          <label>Codigo postal</label>
+          <input type="text" placeholder="#44000" />
+          <label>Calle</label>
+          <input type="text" placeholder="Ingrese la calle de su domicilio" />
+        </div>
+        <button className="btnComprar">
+          <Link className="link">Confirmar compra</Link>
+        </button>
+      </form>
+    </section>
+  );
 };
 
 export default ComprarCarrito;
