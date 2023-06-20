c8bc426e71c8da3c79a673f15f878a5806c5bae4
JoseCastañeda <77752153+josejaimrgcr@users.noreply.github.com>
2023-06-19 19:28:36 -0600

    Iniciamos la validacion del formulario

--- ProyectosWeb/E-commerce/e-commerce/src/components/ComprarCarrito/comprarCarrito.jsx
+++ ProyectosWeb/E-commerce/e-commerce/src/components/ComprarCarrito/comprarCarrito.jsx
@@ -1,8 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "../../css/ComprarCarrito.css";
 
 const ComprarCarrito = () => {
+  //Validacion de formulario
+  const [form, setForm] = useState({
+    nombre: "",
+    correo: "",
+    estadoYMunicio: "",
+    codigoPostal: "",
+    calle: "",
+  });
   return (
     <section className="sectionComprar">
       <h3>Formulario de compra</h3>
@@ -10,18 +18,26 @@ const ComprarCarrito = () => {
         <h5>Datos del usuario</h5>
         <div className="infoUsu">
           <label className="label">Nombre</label>
-          <input type="text" placeholder="Nombre de usuario" />
+          <input type="text" name="nombre" placeholder="Nombre de usuario" />
           <label>Correo</label>
-          <input type="email" placeholder="ejem@gmail.com" />
+          <input type="email" name="correo" placeholder="ejem@gmail.com" />
         </div>
         <h5>Dirección</h5>
         <div className="direccionUsu">
           <label>Estado y Municio</label>
-          <input type="text" placeholder="Aguascalientes, Aguascalientes" />
+          <input
+            type="text"
+            name="estadoYMunicipio"
+            placeholder="Aguascalientes, Aguascalientes"
+          />
           <label>Codigo postal</label>
-          <input type="text" placeholder="#44000" />
+          <input type="text" name="codigoPostal" placeholder="#44000" />
           <label>Calle</label>
-          <input type="text" placeholder="Ingrese la calle de su domicilio" />
+          <input
+            type="text"
+            name="calle"
+            placeholder="Ingrese la calle de su domicilio"
+          />
         </div>
         <button className="btnComprar">
           <Link to={"/confirmarCompra"} className="link">
